b88cb71744f75a7af858d0d5319b6b55e7b39631
Jarek Lipski <loomchild@loomchild.net>
2023-11-26 11:18:56 +0100

    basics

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,13 +7,6 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
